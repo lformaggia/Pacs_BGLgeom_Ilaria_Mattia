1de81dbb19df1c03a3388ae0ea8bc36d6ce48dad
Mattia_Tantardini <mtantar@hotmail.it>
2016-09-11 18:08:52 +0200

    Updating the class point.
    Added:
    - Header guard
    - Copy constructor and assignment operator
    - Partial specialization for the default constructor for 1D, 2D, 3D points.

--- generic_point.hpp
+++ generic_point.hpp
@@ -1,47 +1,48 @@
 /*!
-	@file vertex3d.hpp
-	@brief Defining 3D position property for vertices. \mathcal{N}
+	@file generic_point.hpp
+	@brief Defining 2D or 3D coordition property (coordinates) for vertices.
 	
 	@author Ilaria Speranza and Mattia Tantardini
 
 */
 
+#ifndef HH_GENERIC_POINT_HH
+#define HH_GENERIC_POINT_HH
+
 #include<array>
 #include<iostream>
 
-//! Class template for storing the vertex position in two or three dimentional space.
-template <typename Storage_t, int dim>
+//! Class template for storing the vertex coordition in two or three dimentional space.
+template <int dim, typename Storage_t = double>
 class point {
 	private:
-		std::array<Storage_t, dim> pos;
+		std::array<Storage_t, dim> coord;
 	
 	public:
-		//!Default constructor:
-		point(const Storage_t& x = 0, const Storage_t& y = 0, const Storage_t& z = 0) : pos{{x,y}}  {
-			if(dim == 3)
-				pos[2] = z;
-			if(dim == 2 && z != 0)
-				std::cout << "Warning: third coordinate not used" << std::endl;
-
-		};
+		//! Default constructor for a 1D point:
+		point<1, Storage_t>(const Storage_t& x = 0) : coord{{x}} {};
 		
-		//!Constructor with array: (dovrebbe essere in realtà il copy constructor, quindi possiamo mettere default)
-		point(std::array<Storage_t, dim> m_pos) : pos(m_pos) {};
+		//! Default constructor for a 2D point:
+		point<2, Storage_t>(const Storage_t& x = 0, const Storage_t& y = 0) : coord{{x,y}} {};
 		
-		//Destructor: idem come sopra
+		//! Default constructor for a 3D point:
+		point<3, Storage_t>(const Storage_t& x = 0, , const Storage_t& y = 0, const Storage_t& z = 0) : coord{{x,y,z}} {};
 		
-		//!Const constructor:
+		//! Copy constructor:
+		point(const std::array<Storage_t, dim>& _coord) = default;
 		
-		//!Const copy constructor:
+		//! Assignement operator:
+		point<dim, Storage_t>& operator=(const point<dim, Storage_t>& _point) = default;
+		
 		
 		//!Printing method:
 		void print(){
-			std::cout << "this point is: " << pos[0] << " " << pos[1];
+			std::cout << "this point is: " << coord[0] << " " << coord[1];
 			if(dim == 3)
-				std::cout << " " << pos[2];
+				std::cout << " " << coord[2];
 			std::cout << std::endl; 
 		};
 		
 };
 
-
+#endif // #ifnedf HH_GENERIC_POINT_HH
