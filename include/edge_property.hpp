f0b70ee58fc29b8f3283582812f91e9ba74cfe22
Mattia_Tantardini <mtantar@hotmail.it>
2016-09-15 11:48:31 +0200

    moving header files into subdirectory

---
+++ include/edge_property.hpp
@@ -0,0 +1,10 @@
+#ifndef HH_EDGE_PROPERTY_HH
+#define HH_EDGE_PROPERTY_HH
+
+struct edge_prop_t{
+	double capacity;
+	double length;
+};
+
+
+#endif //#ifndef HH_EDGE_PROPERTY_HH
---
+++ include/edge_property_max_flow.hpp
@@ -0,0 +1,10 @@
+#ifndef HH_EDGE_PROPERTY_MAX_FLOW_HH
+#define HH_EDGE_PROPERTY_MAX_FLOW_HH
+
+struct edge_prop_max_flow_t{
+	double capacity;
+	double residual_capacity; 
+	bool original_edge; 
+};
+
+#endif 
