8ccb1cc7aef27e8025fda5724c9fad3467d86ac6
Mattia_Tantardini <mtantar@hotmail.it>
2016-09-22 19:17:58 +0200

    Finished a first rough implementation of reader base class and its derived class Zunino reader

--- include/reader_Zunino_class.hpp
+++ include/reader_Zunino_class.hpp
@@ -9,14 +9,18 @@
 /*!
 * \file reader_Zunino_class.hpp
 * \author Ilaria Speranza & Mattia Tantardini
-* \date Sep 14, 2016
-* \brief 
+* \date Sept, 2016
+* \brief Implementation of the reader for Zunino file format
 */
 
 #ifndef HH_READER_ZUNINO_CLASS_HH
 #define HH_READER_ZUNINO_CLASS_HH
 
+#include <tuple>
+#include "reader_base_class.hpp"
+
 #include "generic_point.hpp"
+#include "Zunino_edge_property.hpp"		//va inclusa o funziona anche senza?
 
 class reader_Zunino final: public reader_base_class {
 	private:
@@ -30,7 +34,8 @@ class reader_Zunino final: public reader_base_class {
 		reader_Zunino(): reader_base_class(), SRC(), TGT(), src(0), tgt(0), edge_num(0), diam(0.0), length(0.0) {};
 		
 		//! COnstructor: it assigns value only to the variables in reader_base_class, the others in reader_Zunino are defaulted
-		reader_Zunino(unsigned int _num_dummy_lines): 	reader_base_class(_num_dummy_lines),
+		reader_Zunino	(std::string _file_name,
+						unsigned int _num_dummy_lines): reader_base_class(_file_name, _num_dummy_lines),
 														SRC(),
 														TGT(),
 														src(0),
@@ -47,6 +52,38 @@ class reader_Zunino final: public reader_base_class {
 		//! Destructor
 		virtual ~reader_Zunino(){};
 		
+		//! This is the way we read and interpret a file from a Zunino input file format
+		virtual void read_data_from_line(std::istringstream const& temp){
+			temp >> edge_num >> src >> tgt >> diam >> length >> SRC >> TGT;
+		};
+		
+		//! It adds a new vertex in the graph
+		virtual void create_vertex(Vertex_desc & v){
+			v = 
+		};
+		
+		//! It build the graph one edge at a time
+		virtual void build_graph(){
+			//if(vertices_index_already_present) ----> ????
+			//devo automatizzare in qualche modo le due linee sotto!
+			new_source = src; //solo se scelgo vecS vecS nell'adjacency list
+			new_target = tgt; //idem come sopra
+			std::tie(new_edge, edge_inserted) = boost::add_edge(new_source, new_target, G);		//add_edge(new_source, new_target, G)
+			if(!edge_inserted){
+				//succede qualcosa
+			}
+		};
+		
+		//! It adds the right properties to the vertices just added
+		virtual void give_vertex_properties(Vertex_desc const& new_source, Vertex_desc const& new_target){
+			G[new_source] = SRC;
+			G[new_target] = TGT;
+		};
+		
+		virtual void give_edge_properties(Edge_desc const& e){
+			G[e].capacity = diam;
+			G[e].length = length;
+		};
 
 };
 
--- include/reader_base_class.hpp
+++ include/reader_base_class.hpp
@@ -9,26 +9,34 @@
 /*!
 * \file reader_base_class.hpp
 * \author Ilaria Speranza & Mattia Tantardini
-* \date Sep 14, 2016
-* \brief 
+* \date Sept, 2016
+* \brief Base abstract class to read input file and creating the graph
 */
 
 #ifndef HH_READER_BASE_CLASS_HH
 #define HH_READER_BASE_CLASS_HH
 
+#include <string>
+#include <ifstream>
+#include <istringstream>
+
+#include <boost/graph/graph_traits.hpp>
+
 template <typename Graph>
 class reader_base_class{
-	protected:
-		Graph G;		//maybe pointer???
-		unsigned int num_dummy_lines;
-		
 	public:
 	
+		typedef typename graph_traits<Graph>::vertex_descriptor Vertex_desc;
+		typedef typename graph_traits<Graph>::edge_descriptor Edge_desc;
+	
 		//! Default constructor
-		reader_base_class(): G(0), num_dummy_lines(0) {};
+		reader_base_class(): G(0), file_name(), num_dummy_lines(0), line() {};
 		
 		//! Constructor: assign only num_dummy_lines, empty graph
-		reader_base_class(unsigned int _num_dummy_lines):  G(0), num_dummy_lines(_num_dummy_lines) {};
+		reader_base_class(std::string _file_name, unsigned int _num_dummy_lines):	G(0),
+																					file_name(_file_name),
+																					num_dummy_lines(_num_dummy_lines),
+																					line() {};
 		
 		//! Default copy constructor
 		reader_base_class(reader_base_class const&) = default;
@@ -42,6 +50,10 @@ class reader_base_class{
 		//! Getting the graph (once built)
 		Graph get_graph(){ return G; };		//così però lo passo per copia.. se è grosso?
 		
+		//! Read the input file
+		void read_input_file();
+		
+		//! It ignores the first n lines, that are headers, in the input file
 		// la metto qui? Perché fa sempre la stessa cosa, cambia solo la num_dummy_line
 		virtual void ignore_dummy_lines(std::ifstream const& file){
 			std::string dummy;
@@ -49,15 +61,58 @@ class reader_base_class{
 				getline(file, dummy);
 		};
 		
-		virtual void read_data_from_line(std::istringstream temp) = 0;
+		//! It describes how to read each line in the input file, and in which variables to store the data
+		virtual void read_data_from_line(std::istringstream const& temp) = 0;
 		
-		virtual void create_vertex() = 0;
+		//! It adds a new vertex in the graph and returns the corrispondent vertex_descriptor by reference
+		virtual void create_vertex(Vertex_desc & v) = 0;
 		
-		virtual void give_vertex_properties() = 0;
+		//! It build the graph one edge at a time, called many times from an external loop
+		virtual void build_graph() = 0;
+		
+		virtual void give_vertex_properties(Vertex_desc const& new_source, Vertex_desc const& new_target) = 0;
 		
 		virtual void create_edge() = 0;
 		
 		virtual void give_edge_property() = 0;
+		
+	protected:
+		Graph G;		//maybe pointer???
+		std::string file_name;
+		unsigned int num_dummy_lines;
+		std::string line;
+		Vertex_desc new_source;
+		Vertex_desc new_target;
+		Edge_desc new_edge;
+		bool edge_inserted;
+		bool vertices_index_already_present		//va inizializzato!		
+};
+
+template <typename Graph>
+void reader_base_class<Graph>::read_input_file(){
+
+	std::ifstream file(file_name.c_str());
+	
+	//controllo che apertura file sia andata a buon fine
+	
+	this->ignore_dummy_lines(file, num_dummy_lines);
+	
+	while(!file.fail() && !file.eof()){
+		std::getline(file, line);
+		if(line.empty())
+			continue;
+		std::istringstream temp(s);
+		this->read_data_from_line(temp);
+		if(!temp.fail())
+			//aggiungo nodi e arco corrente
+			this->build_graph();
+		
+			//creo i nodi
+			//this->create_vertex(source);
+			
+			this->give_vertex_properties();
+			this->give_edge_properties();
+	}
 
 };
 
