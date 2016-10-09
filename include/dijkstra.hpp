bcde0d3fdbee5750f2f27856060c3373fb623d44
10396201 <ilaria.speranza@mail.polimi.it>
2016-10-09 10:01:36 +0200

    debug mode in Makefile.inc and dijkstra algorithm

---
+++ include/dijkstra.hpp
@@ -0,0 +1,29 @@
+/*======================================================================
+                        "[nome_progetto]"
+        Course on Advanced Programming for Scientific Computing
+                      Politecnico di Milano
+                          A.Y. 2015-2016
+                  
+         Copyright (C) 2016 Ilaria Speranza & Mattia Tantardini
+======================================================================*/
+/*! 
+	@file   dijkstra.hpp
+	@author Ilaria Speranza & Mattia Tantardini
+	@date   Sep 2016.
+	@brief  Solves the single-source shortest-paths problem on a weighted, directed graph with non-negative edge weights.
+	@detail
+*/
+
+#ifndef HH_DIJKSTRA_HH
+#define HH_DIJKSTRA_HH
+
+#include "boost/graph/dijkstra_shortest_paths.hpp"
+#include "dijkstra_imp.hpp"
+
+template<typename Graph>
+void dijkstra(typename boost::graph_traits<Graph>::vertex_descriptor v, property_maps...)
+
+
+
+
+#endif
---
+++ include/dijkstra_imp.hpp
@@ -0,0 +1,20 @@
+/*======================================================================
+                        "[nome_progetto]"
+        Course on Advanced Programming for Scientific Computing
+                      Politecnico di Milano
+                          A.Y. 2015-2016
+                  
+         Copyright (C) 2016 Ilaria Speranza & Mattia Tantardini
+======================================================================*/
+/*! 
+	@file   dijkstra_imp.hpp
+	@author Ilaria Speranza & Mattia Tantardini
+	@date   Sep 2016.
+	@brief  Solves the single-source shortest-paths problem on a weighted, directed graph with non-negative edge weights.
+	@detail
+*/
+
+#ifndef HH_DIJKSTRA_IMP_HH
+#define HH_DIJKSTRA_IMP_HH
+
+#endif
