55123f9bc53f42918b5620c2b0369b042da94d48
Mattia_Tantardini <mtantar@hotmail.it>
2016-10-13 16:02:46 +0200

    Trying to build an example of application of new_reader_class

--- include/new_reader_class.hpp
+++ include/new_reader_class.hpp
@@ -21,6 +21,7 @@
 
 #include <string>
 #include <fstream>
+#include <iostream>
 
 //! Forward declaration of the struct that will contain the data readed from a line
 struct data_from_line{};
@@ -45,22 +46,33 @@ class new_reader_class{
 		new_reader_class & (new_reader_class const&) = default;
 		
 		//! Set the input file to read
-		void set_input(std::string _filename){
+		virtual void set_input(std::string _filename){
 			filename = _filename;
 			in_file.open(filename);
 			file_is_opened = true;
 		}
 		
-		//! Ignore n lines of the input code that the user knows he has not to read
-		void ignore_dummy_lines(unsigned int const& n){
-			
+		/*! 
+			@brief Ignore n lines of the input code that the user knows he has not to read
+			@remark It sets the file stream n lines after the previous position
+		*/
+		virtual void ignore_dummy_lines(unsigned int const& n){
+			if(!file_is_opened){
+				std::cerr << "Error: input file not opened for reading" << std::endl;
+				return;
+			}
+			std::string dummy;
+			for(std::size_t i = 0; i < n; ++i)
+				std::getline(file, dummy);
 		}
+		
+		//! Reads the data from one single line. It has to be specified by the user
+		virtual data_from_line read_line() = 0;
 	
 	protected:
 		std::string filename;
 		std::ifstream in_file;
 		bool file_is_opened;
 };	//new_reader_class
-
 
 #endif //HH_NEW_READER_CLASS_HH
