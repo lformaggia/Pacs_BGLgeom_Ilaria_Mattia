aa0b182d1f6db0001f851e085f791dd7912ca232
Mattia_Tantardini <mtantar@hotmail.it>
2016-11-13 18:34:58 +0100

    Trying to overload operator>> for eigen, not succesfull

--- include/point.hpp
+++ include/point.hpp
@@ -16,15 +16,34 @@
 #ifndef HH_POINT_HH
 #define HH_POINT_HH
 
-#include<Eigen/Dense>
-
+#include <iostream>
+#include <Eigen/Dense>
 
 namespace BGLgeom{
 /*!
 	@brief Alias template: point<N> is the following Eigen::array
 */
 template <unsigned int N>
-using point = Eigen::Matrix<double,N,1>;
-} //namespace
+using point = Eigen::Matrix<double,1,N>;
 
+template<typename Derived>
+std::istream & operator>>(std::istream & s, Eigen::DenseBase<Derived> & m){
+	for(std::size_t i = 0; i < m.rows(); ++i){
+		for(std::size_t j = 0; j < m.cols(); ++j){
+		    s >> m(i,j); 
+	   }
+	}
+	return s;
+}
+
+
+/*
+//! Overload of operator>> for BGLgeom::point<N>
+template <unsigned int N>
+std::istream & operator>>(std::istream & in, Eigen::Matrix<double,1,N> & P){
+	for(std::size_t i = 0; i < N; ++i)
+		in >> P[i];
+}
+*/
+} //namespace
 #endif //HH_GENERIC_POINT_HH
--- src/test_generic_point.cpp
+++ src/test_generic_point.cpp
@@ -1,42 +1,19 @@
 //#include "generic_point.hpp"
 #include <iostream>
+#include <fstream>
+#include "point.hpp" 
 
-#include "point2D.hpp"
-#include "point3D.hpp" 
+using namespace BGLgeom;
 
 int main(){
 	
-	point2D P1;
-	std::cout << "P1: " << P1 << std::endl;
+	std::ifstream infile;
+	infile.open("../data/test_point.txt");
+	point<3> P1;
+	point<3> P2;
 	
-	point2D P2(3,5);
-	std::cout << "P2: " << P2 << std::endl;
-	P2.set_x(4);
-	std::cout << "P2: " << P2 << std::endl;
-	P2.set_y(4);
-	std::cout << "P2: " << P2 << std::endl;
-	P2.set(3,3);
-	std::cout << "P2: " << P2 << std::endl;
-	bool boh = P1 < P2;
-	std::cout << "P1 < P2: " << boh << std::endl;
-	
-	std::cout << std::endl << std::endl;
-	
-	point3D P3;
-	std::cout << "P3: " << P3 << std::endl;
-	
-	point3D P4(3,5,7);
-	std::cout << "P4: " << P4 << std::endl;
-	P4.set_x(4);
-	std::cout << "P4: " << P4 << std::endl;
-	P4.set_y(4);
-	std::cout << "P4: " << P4 << std::endl;
-	P4.set_z(4);
-	std::cout << "P4: " << P4 << std::endl;
-	P4.set(0,0,3);
-	std::cout << "P4: " << P4 << std::endl;
-	bool boh2 = P3 < P4;
-	std::cout << "P3 < P4: " << boh2 << std::endl;
+	infile >> P1 >> P2;
+	std::cout << "punto 1: " << P1 << ", punto 2: " << P2 << std::endl;
 	
 	return 0;
 }
