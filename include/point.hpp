cf4d22b0c4c2463f9171fe79e717f1d62488079a
Mattia_Tantardini <mtantar@hotmail.it>
2016-11-14 02:17:56 +0100

    Coming back to column vector part 2

--- include/point.hpp
+++ include/point.hpp
@@ -39,7 +39,7 @@ std::istream & operator>>(std::istream & s, Eigen::DenseBase<Derived> & m){
 */
 
 template <unsigned int N>
-void read_point(std::istream & in, Eigen::Matrix<double,1,N> & P){
+void read_point(std::istream & in, Eigen::Matrix<double,N,1> & P){
 	double a;
 	for(std::size_t i = 0; i < N; ++i){
 		in >> a;
@@ -49,13 +49,13 @@ void read_point(std::istream & in, Eigen::Matrix<double,1,N> & P){
 
 
 //! Overload of operator>> for BGLgeom::point<2>
-std::istream & operator>>(std::istream & in, Eigen::Matrix<double,1,2> & P){
+std::istream & operator>>(std::istream & in, Eigen::Matrix<double,2,1> & P){
 	for(std::size_t i = 0; i < P.cols(); ++i)
 		in >> P(0,i);
 }
 
 //! Overload of operator>> for BGLgeom::point<3>
-std::istream & operator>>(std::istream & in, Eigen::Matrix<double,1,3> & P){
+std::istream & operator>>(std::istream & in, Eigen::Matrix<double,3,1> & P){
 	std::cout << P.rows() << ", " << P.cols() << std::endl;
 	double a;
 	for(int i = 0; i < (int)P.cols(); ++i){
@@ -65,17 +65,18 @@ std::istream & operator>>(std::istream & in, Eigen::Matrix<double,1,3> & P){
 	}
 }
 
-/*	Funzionano, volendo
+/*	//Funzionano, volendo. Non è vero.
 //! Overload of operator<< for BGLgeom::point<2>
-std::ostream & operator<<(std::ostream & out, Eigen::Matrix<double,1,2> const& P){
+std::ostream & operator<<(std::ostream & out, Eigen::Matrix<double,2,1> const& P){
 	out << "(" << P(0,1) << "," << P(0,2) << ")";
 }
 
 //! Overload of operator<< for BGLgeom::point<3>
-std::ostream & operator<<(std::ostream & out, Eigen::Matrix<double,1,3> const& P){
+std::ostream & operator<<(std::ostream & out, Eigen::Matrix<double,3,1> const& P){
 	out << "(" << P(0,0) << "," << P(0,1) << ";" << P(0,2) << ")";
 }
 */
+
 
 } //namespace
 #endif //HH_GENERIC_POINT_HH
