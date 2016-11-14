bc51999ed2c1f899df01910779ad45b4e5fa1597
Mattia_Tantardini <mtantar@hotmail.it>
2016-11-14 09:35:59 +0100

    Developing writer

--- libgeograph/include/writer_ASCII.hpp
+++ libgeograph/include/writer_ASCII.hpp
@@ -22,20 +22,22 @@
 #include <fstream>
 #include <cstdlib>
 #include <initializer_list>
+#include <tuple>
+#include <boost/graph/adjacency_list.hpp>
 
 namespace BGLgeom{
 
 /*!
 	@brief Class handling output operations for a tabular type file
 */
-template <>
+template <typename Graph>
 class writer_ASCII{
 	public:
 		//! Default constructor
-		writer_ASCII() : filename(), out_file(){};
+		writer_ASCII() : out_file(){};
 		
 		//! Constructor
-		writer_ASCII(std::string _filename) : filename(_filename), out_file(){
+		writer_ASCII(std::string _filename) : out_file(){
 			try{
 				out_file.open(filename);
 			} catch(std::exception & e) {
@@ -43,6 +45,9 @@ class writer_ASCII{
 				exit(EXIT_FAILURE);
 			}
 		}
+		
+		//! Destructor
+		virtual ~writer_ASCII(){};
 		
 		//Ha senso?
 		//! Copy constructor
@@ -52,7 +57,7 @@ class writer_ASCII{
 		
 		//! Set the output file
 		virtual void set_output(std::string _filename){
-			filename = _filename;
+			out_file.close();	//I close the previous file I opened in the constructor
 			try{
 				out_file.open(filename);
 			} catch(std::exception & e) {
@@ -66,19 +71,57 @@ class writer_ASCII{
 			@detail Implemented using initializer_list, so it can accept any number of arguments
 		*/
 		virtual void put_headers(std::initializer_list<std::string> args){
-		
+			typename std::initializer_list<string>::iterator init_list_it, init_list_end;
+			init_list_it = args.begin();
+			init_list_end = args.end();
+			for( ; init_list_it != init_list_end; ++init_list_it){
+				out_file << *init_list_it; //<< "\t";
+				if(init_list_it != init_list_end)	//per non mettere uno spazio in fondo. Da migliorare con counter
+					out_file << " ";
+			}
+			out_file << std::endl;
 		}
 		
-		//! It writes a line of data
-		virtual void write_line() = 0;
+		/*!
+			@brief It writes a line of data
+		*/
+		virtual void write_line(Graph const& G, typename boost::graph_traits<Graph>::edge_descriptor const& e){
+			typename boost::graph_traits<Graph>::vertex_descriptor src, tgt;
+			src = boost::source(*e_it, G);
+			tgt = boost::target(*e_it, G);
+			out_file << G[src] << " " << G[tgt] << " " << G[*e_it] << std::endl;
+		}
 		
-		//! It writes all data from the edge and vertex properties
+		/*!
+			@brief It writes all data from the edge and vertex properties
+			@detail It writes all properties of the graph, one line for each edge, 
+					giving vertex properties as source and target properties of
+					that edge
+			@pre All properties must have an overload of the output operator in which
+					all data concerning the property are displayed in one line, according
+					to some known and predefined order
+		*/
 		//Ha senso?(Se ci sono output strani delle cose base è un casino, o vuol dire che devo preimpostare tutto in quest'ottica). Come si fa? 
-		virtual void write_properties() = 0;	
+		virtual void write_properties(Graph const& G){
+			//gestire gli header direttamente da qui, o in data_structure?
+			typename boost::graph_traits<Graph>::edge_iterator e_it, e_end;
+			typename boost::graph_traits<Graph>::vertex_descriptor src, tgt;
+			unsigned int n = num_edges(G);
+			unsigned int i = 0;
+			for(std::tie(e_it, e_end) = boost::edges(G); e_it != e_end; ++e_it){
+				++i;
+				src = boost::source(*e_it, G);
+				tgt = boost::target(*e_it, G);
+				// We print, for each edge: properties of source, properties of target, properties of the edge
+				out_file << G[src] << " " << G[tgt] << " " << G[*e_it];
+				if(i != n)
+					out_file << std::endl;
+			}	//for
+		};	
 		
 	protected:
-		//! The name of the output file
-		std::string filename;	//Non serve a niente avere il filename dentro in realtà, gli si può passare sempre diretto così sono anche sicuro che apro sempre e che ha un nome.
+		// The name of the output file
+		//std::string filename;	//Non serve a niente avere il filename dentro in realtà, gli si può passare sempre diretto così sono anche sicuro che apro sempre e che ha un nome.
 		//! The stream associated to the output file
 		std::ofstream out_file;
 
