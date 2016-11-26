b5eb954bcfb7ced2c6285f83586372b5f6eb0790
Mattia_Tantardini <mtantar@hotmail.it>
2016-11-26 19:14:48 +0100

    Fixed comments and added working input and output operators

--- libBGLgeom/include/point.hpp
+++ libBGLgeom/include/point.hpp
@@ -1,5 +1,5 @@
 /*======================================================================
-                        "[nome_progetto]"
+                        "BGLgeom library"
         Course on Advanced Programming for Scientific Computing
                       Politecnico di Milano
                           A.Y. 2015-2016
@@ -10,7 +10,7 @@
 	@file point.hpp
 	@author Ilaria Speranza and Mattia Tantardini
 	@date Sept, 2016
-	@brief Alias template of point
+	@brief Description of a point and utilities
 */
 
 #ifndef HH_POINT_HH
@@ -21,62 +21,38 @@
 
 namespace BGLgeom{
 /*!
-	@brief Alias template: point<N> is the following Eigen::array
+	@brief Alias template for a point in N-th dimensional space, using Eigen::Matrix
 */
 template <unsigned int N>
 using point = Eigen::Matrix<double,1,N>;
 
-/*
+//! Overload of the input operator for Eigen Matrices
 template<typename Derived>
-std::istream & operator>>(std::istream & s, Eigen::DenseBase<Derived> & m){
+std::istream & operator>>(std::istream & in, Eigen::DenseBase<Derived> & m){
 	for(std::size_t i = 0; i < m.rows(); ++i){
 		for(std::size_t j = 0; j < m.cols(); ++j){
-		    s >> m(i,j); 
+		    in >> m(i,j); 
 	   }
 	}
-	return s;
+	return in;
 }
+
+/*! 
+	@brief Overload of the output operator for Eigen Matrices
+	@detail It prints each coordinate separated by a tab, in order to suite
+			the expected output for the pts file format
 */
-
-template <unsigned int N>
-void read_point(std::istream & in, Eigen::Matrix<double,1,N> & P){
-	double a;
-	for(std::size_t i = 0; i < N; ++i){
-		in >> a;
-		P(0,i) = a;
+template<typename Derived>
+std::ostream & operator<<(std::ostream & out, Eigen::DenseBase<Derived> & m){
+	for(std::size_t i = 0; i < m.rows(); ++i){
+		for(std::size_t j = 0; j < m.cols()-1; ++j){
+		    out << m(i,j) << "\t"; 
+	   }
 	}
+	out << m(m.rows()-1,m.cols()-1);
+	return out;
 }
 
+} //BGLgeom
 
-//! Overload of operator>> for BGLgeom::point<2>
-std::istream & operator>>(std::istream & in, Eigen::Matrix<double,1,2> & P){
-	for(std::size_t i = 0; i < P.cols(); ++i)
-		in >> P(0,i);
-}
-
-//! Overload of operator>> for BGLgeom::point<3>
-std::istream & operator>>(std::istream & in, Eigen::Matrix<double,1,3> & P){
-	std::cout << P.rows() << ", " << P.cols() << std::endl;
-	double a;
-	for(int i = 0; i < (int)P.cols(); ++i){
-		std::cout << in.peek() << std::endl;
-		in >> a;
-		P(0,i) = a;
-	}
-}
-
-/*	//Funzionano, volendo. Non è vero.
-//! Overload of operator<< for BGLgeom::point<2>
-std::ostream & operator<<(std::ostream & out, Eigen::Matrix<double,1,2> const& P){
-	out << "(" << P(0,1) << "," << P(0,2) << ")";
-}
-
-//! Overload of operator<< for BGLgeom::point<3>
-std::ostream & operator<<(std::ostream & out, Eigen::Matrix<double,1,3> const& P){
-	out << "(" << P(0,0) << "," << P(0,1) << ";" << P(0,2) << ")";
-}
-*/
-
-
-} //namespace
 #endif //HH_GENERIC_POINT_HH
