9ada780d36f69e0f703440bae1851430fbf07a7f
Mattia_Tantardini <mtantar@hotmail.it>
2016-11-28 16:43:43 +0100

    Test for writer_pts

---
+++ libBGLgeom/src/test_writer_pts.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+#include <fstream>
+#include <string>
+#include <vector>
+
+#include <boost/graph/adjacency_list.hpp>
+#include "writer_pts.hpp"
+#include "data_structure.hpp"
+
+using namespace boost;
+using namespace BGLgeom;
+
+int main(){
+	adjacency_list<vecS,vecS,undirectedS,Vertex_base_property,no_property> G;
+	std::vector<Vertex_base_property::point_t> mesh;
+	
+	//initializing mesh
+	
+
+	return 0;
+}
