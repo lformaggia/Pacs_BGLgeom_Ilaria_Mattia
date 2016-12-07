aabb53f851e64996a2c6345f464e6249f0009f8a
Mattia_Tantardini <mtantar@hotmail.it>
2016-12-07 16:10:06 +0100

    Implemented relational operators for points

--- libBGLgeom/include/point.hpp
+++ libBGLgeom/include/point.hpp
@@ -27,31 +27,80 @@ template <unsigned int N>
 using point = Eigen::Matrix<double,1,N>;
 
 //! Overload of the input operator for Eigen Matrices
-template<typename Derived>
-std::istream & operator>>(std::istream & in, Eigen::DenseBase<Derived> & m){
-	for(std::size_t i = 0; i < m.rows(); ++i){
-		for(std::size_t j = 0; j < m.cols(); ++j){
+template <typename Derived>
+std::istream &
+operator>>(std::istream & in, Eigen::DenseBase<Derived> & m){
+	for(std::size_t i = 0; i < static_cast<unsigned int>(m.rows()); ++i){
+		for(std::size_t j = 0; j < static_cast<unsigned int>(m.cols()); ++j){
 		    in >> m(i,j); 
 	   }
 	}
 	return in;
 }
 
-/*! 
-	@brief Overload of the output operator for Eigen Matrices
-	@detail It prints each coordinate separated by a tab, in order to suite
-			the expected output for the pts file format
-*/
-template<typename Derived>
-std::ostream & operator<<(std::ostream & out, Eigen::DenseBase<Derived> & m){
-	for(std::size_t i = 0; i < m.rows(); ++i){
-		for(std::size_t j = 0; j < m.cols()-1; ++j){
-		    out << m(i,j) << "\t"; 
+//! Overload of the output operator for Eigen Matrices
+template <typename Derived>
+std::ostream &
+operator<<(std::ostream & out, Eigen::DenseBase<Derived> & m){
+	for(std::size_t i = 0; i < static_cast<unsigned int>(m.rows()); ++i){
+		for(std::size_t j = 0; j < static_cast<unsigned int>(m.cols())-1; ++j){
+		    out << m(i,j) << " "; 
 	   }
 	}
 	out << m(m.rows()-1,m.cols()-1);
 	return out;
 }
+
+// Write_pts
+// Write_ASCII
+
+/*!
+	 @brief Operator< overloading			 
+	 @detail Point1 < Point2 if Point1.x is smaller than Point2.x;
+	 		 if they are equal, compare in the same waythe y coordinate, and so on.		
+*/
+template <typename Derived>
+bool
+operator< (Eigen::DenseBase<Derived> const& P1, Eigen::DenseBase<Derived> const& P2){							
+	for(std::size_t i = 0; i < static_cast<unsigned int>(P1.cols()); ++i){
+		if(P1(0,i) < P2(0,i))
+			return true;
+		else if (P1(0,i) > P2(0,i))
+			return false;
+	}			
+	return false;		//if they are equal	
+}
+
+/*!
+	@brief Operator> overloading			
+	@detail It is the negation of operator<
+*/
+template <typename Derived>
+bool
+operator> (Eigen::DenseBase<Derived> const& P1, Eigen::DenseBase<Derived> const& P2){							
+	return !(operator<(P1,P2));	
+}
+
+/*!
+	@brief Operator== overloading			
+	@detail It checks if all the coordinates are equal
+*/
+template <typename Derived>
+bool
+operator== (Eigen::DenseBase<Derived> const& P1, Eigen::DenseBase<Derived> const& P2){
+	return P1 == P2;
+}
+
+/*!
+	@brief Operator!= overloading			
+	@detail It checks if the two points are different
+*/
+template <typename Derived>
+bool
+operator!= (Eigen::DenseBase<Derived> const& P1, Eigen::DenseBase<Derived> const& P2){
+	return P1 != P2;
+}
+
 
 } //BGLgeom
 
--- libBGLgeom/src/test_point.cpp
+++ libBGLgeom/src/test_point.cpp
@@ -11,17 +11,14 @@ int main(){
 	point<3> P1;
 	point<3> P2;
 	
-	infile >> P1 >> P2;	//tutto in fila non va. Perché?
-	//std::cout << "Hello" << std::endl;
+	infile >> P1 >> P2;
 	std::cout << P1 << ", " << std::endl;
-	//infile >> P2;
 	std::cout << P2 << ", " << std::endl;
 	
-	/*
-	read_point<3>(infile,P1);
-	read_point<3>(infile,P2);
-	std::cout << P1 << ", " << P2 << std::endl;
-	*/
+	std::cout << "Comparison: P1 < P2? " << operator<(P1,P2) << std::endl;
+	std::cout << "Comparison: P1 > P2? " << (P1 > P2) << std::endl;
+	std::cout << "Comparison: P1 == P2? " << (P1 == P2) << std::endl;
+	std::cout << "Comparison: P1 != P2? " << (P1 != P2) << std::endl;
 	
 	return 0;
 }
