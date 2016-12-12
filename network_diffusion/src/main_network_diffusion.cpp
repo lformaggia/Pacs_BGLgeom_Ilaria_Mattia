f2aba4bc9a5ba16ddb65d769148bed5001199a31
Mattia_Tantardini <mtantar@hotmail.it>
2016-12-12 16:40:34 +0100

    Renamed main

---
+++ network_diffusion/src/main_network_diffusion.cpp
@@ -0,0 +1,16 @@
+/*!
+	@file main_Zunino.cpp
+	@author Ilaria Speranza & Mattia Tantardini
+	@date Sept, 2016
+	@brief Source code for Zunino example.
+	@detail
+*/
+
+#include <iostream>
+#include <string>
+
+
+int main(){
+
+	return 0;
+}
