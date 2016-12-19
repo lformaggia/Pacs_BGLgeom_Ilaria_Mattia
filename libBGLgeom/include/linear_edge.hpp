e1039e2700a62ecd1a0403f2c9912156c72da587
Mattia_Tantardini <mtantar@hotmail.it>
2016-12-19 18:32:28 +0100

    implementing evaluation in vectors of parameters

--- libBGLgeom/include/linear_edge.hpp
+++ libBGLgeom/include/linear_edge.hpp
@@ -47,9 +47,9 @@ class linear_edge : public BGLgeom::edge_geometry<dim>, public parametric_mesh_g
 		//std::unique_ptr<parametric_mesh_generator> param_mesh;
 
 	public:
-		//using vect_Eigen = Eigen::Matrix<double,1,dim>;
 		using point = BGLgeom::point<dim>;
 		using vect_pts = std::vector<point>;
+		using vect_double = std::vector<double>;
 		
 		//! Default constructor 
 		linear_edge() : SRC(), TGT() {};	
@@ -97,7 +97,7 @@ class linear_edge : public BGLgeom::edge_geometry<dim>, public parametric_mesh_g
   			@brief It evaluates a vector of values for the parameter
   		*/
   		vect_pts
-  		operator() (std::vector<double> const& t) const {
+  		operator() (vect_double const& t) const {
     		vect_pts P_vect(t.size());
    			for (std::size_t i = 0; i < t.size(); ++i)
    				P_vect[i] = this->operator()(t[i]); //(TGT-SRC)*t[i] + SRC;   			
@@ -110,7 +110,7 @@ class linear_edge : public BGLgeom::edge_geometry<dim>, public parametric_mesh_g
 		
 		//! Evaluates the first derivatives in a vector of values of the parameter
 		vect_pts
-		first_der(std::vector<double> const& t){
+		first_der(vect_double const& t){
 			vect_pts Fder(t.size());
 			for (std::size_t i = 0; i < t.size(); ++i)
    				Fder[i] = this->first_der(); //(TGT-SRC)*t[i] + SRC;   			
@@ -119,7 +119,13 @@ class linear_edge : public BGLgeom::edge_geometry<dim>, public parametric_mesh_g
 		
 		//! Evaluates the second derivative of the line (of course returns zero!)
 		point
-		second_der(const double & t = 0) { return point::Zero(); }		
+		second_der(const double & t = 0) { return point::Zero(); }
+		
+		//! Evaluates the second derivative of the line in a vector of parameters
+		vect_pts
+		second_der(vect_double const& t){
+			return vect_pts(t.size(), point::Zero());
+		}
 
 		/*! 
 			@brief Curvilinear abscissa.
@@ -139,9 +145,24 @@ class linear_edge : public BGLgeom::edge_geometry<dim>, public parametric_mesh_g
 			return (TGT-SRC).norm() * t;
 		}
 		
+		//! Evaluates the cuvilinear abscissa in a vector of parameters
+		vect_double
+		curv_abs(vect_double const& t){
+			vect_double C(t.size());
+			for(std:size_t i = 0; i < t.size(); ++i)
+				C[i] = this->curv_abs(t[i]);
+			return C;
+		}
+		
 		//! Evaluates the curvature of the line (of course zero again!)
 		double
 		curvature(const double & x) { return 0; }
+		
+		//! Evaluates the curvature of the line in a vector of parameters
+		vect_double
+		curvature(vect_double const& t){
+			return vect_double(t.size(),0.0);
+		}
 		
 			/*	
 		//! Getting the real points from the parametric mesh
@@ -150,6 +171,30 @@ class linear_edge : public BGLgeom::edge_geometry<dim>, public parametric_mesh_g
 			return this->operator()(this->p_mesh);
 		}
 		*/
+		
+		/*! 
+ 			@brief Creating a mesh on the edge
+ 			@detail SRC and TGT are included in the mesh points
+ 			@param n Number of intervals
+ 		*/
+ 		/*
+		std::vector<point>
+		vect_pts
+ 		uniform_mesh_boh(unsigned int const& n) {
+ 			double h_abscissa = static_cast<double>(1/n);
+ 			double s = 0;
+ 			vect_double = param_mesh;
+ 			param_mesh.resize(n+1);
+			vect_pts retval;
+ 			param_mesh.push_back(0);
+ 			for(std::size_t i=0; i < n-1; ++i){	//n_points-1 per non includere già qui TGT
+ 				s += h_abscissa;
+ 				retval.emplace_back(point(this->operator()(s)));
+ 			}
+ 			param_mesh.push_back(1);
+ 			return retval;			
+ 		}
+ 		*/
 		
 		//! Overload of operator<<
 		friend std::ostream & operator << (std::ostream & out, linear_edge<dim> & edge) {
