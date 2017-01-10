cd1036fe2217428f7181426488c4ebf49f6e6c9c
Mattia_Tantardini <mtantar@hotmail.it>
2017-01-10 18:09:50 +0100

    Updated name of edge_base_property

--- libBGLgeom/include/test_writer_ASCII.hpp
+++ libBGLgeom/include/test_writer_ASCII.hpp
@@ -1,3 +1,20 @@
+/*======================================================================
+                        "BGLgeom library"
+        Course on Advanced Programming for Scientific Computing
+                      Politecnico di Milano
+                          A.Y. 2015-2016
+                  
+         Copyright (C) 2016 Ilaria Speranza & Mattia Tantardini
+======================================================================*/
+/*!
+	@file	test_writer_ASCII.hpp
+	@author	Ilaria Speranza & Mattia Tantardini
+	@date	Jan, 2017
+	@brief	Definition of the concrete class that inherits from writer_ASCII
+	
+	@detail	We define a test edge property for the graph. Then the concrete 
+			writer for that type of graph is defined.
+*/
 
 #ifndef HH_TEST_WRITER_ASCII_HH
 #define HH_TEST_WRITER_ASCII_HH
@@ -5,29 +22,33 @@
 #include <vector>
 #include <string>
 #include "data_structure.hpp"
+#include "graph_builder.hpp"
 #include "writer_ASCII.hpp"
 #include "linear_geometry.hpp"
 
 namespace{
 
-struct Edge_prop_ASCII : public BGLgeom::Edge_base_property_static<BGLgeom::linear_geometry<2>,2> {
+//! Edge property we will assign to the graph
+struct Edge_prop_ASCII : public BGLgeom::Edge_base_property<BGLgeom::linear_geometry<2>,2> {
 	double foo_double;
 	std::vector<int> foo_vector;
 	std::string foo_string;
 	
 	//! Default constructor
-	Edge_prop_ASCII() : BGLgeom::Edge_base_property_static<BGLgeom::linear_geometry<2>,2>(),
+	Edge_prop_ASCII() : BGLgeom::Edge_base_property<BGLgeom::linear_geometry<2>,2>(),
 						foo_double(),
 						foo_vector(),
 						foo_string() {};
 };	//Edge_prop_ASCII
 
+//! Concrete writer_ASCII for the test, and specific for the properties we will have on the graph
 template <typename Graph>
 class test_writer_ASCII : public BGLgeom::writer_ASCII<Graph> {
 	public:
 		//! Constructor
 		test_writer_ASCII(std::string _filename) : BGLgeom::writer_ASCII<Graph>(_filename){};
 		
+		//! Defining abstract method
 		void
 		write_data(BGLgeom::Edge_desc<Graph> const& e, Graph const& G){
 			BGLgeom::Vertex_desc<Graph> src = boost::source(e,G);
